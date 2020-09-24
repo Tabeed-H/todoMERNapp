c2b902ea252333e3b2e7d6182cf516473798865f
Tabeed Hameed <tabeedhameed121@gmail.com>
2020-09-24 19:40:59 +0530

    first commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Todo from "./components/Todo";
 import AddTodo from "./components/addTodo";
+import About from "./components/pages/About";
 import "./App.css";
 const uuid = require("uuid");
 //import ReactDOM from "react-dom";
@@ -47,19 +49,39 @@ class App extends Component {
     //console.log(this.state.todos);
 
     return (
-      <div className="App">
-        <div className="header">
-          <h2>ToDo List</h2>
+      <Router>
+        <div className="App">
+          <div className="header">
+            <h2>ToDo List</h2>
+            <Link className="links" to="/">
+              home
+            </Link>{" "}
+            |{" "}
+            <Link className="links" to="/about">
+              About
+            </Link>
+          </div>
+          <div className="container">
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <div className="list">
+                    <Todo
+                      todos={this.state.todos}
+                      markComplete={this.markComplete}
+                      delTodo={this.delTodo}
+                    />
+                  </div>
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-        <AddTodo addTodo={this.addTodo} />
-        <div className="list">
-          <Todo
-            todos={this.state.todos}
-            markComplete={this.markComplete}
-            delTodo={this.delTodo}
-          />
-        </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ todo/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>thanks for using the app</p>
+        <p>version v1.0.0</p>
+      </div>
+    );
+  }
+}
+
+export default About;
